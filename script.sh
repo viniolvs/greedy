09c7442154e8ac13d1b9fa16647d46459e7ba953
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2023-02-19 17:03:53 -0300

    removed biggest test

--- script.sh
+++ script.sh
@@ -3,8 +3,9 @@ DIR="entradas"
 MODO=$1
 
 echo "Aulas,Salas,Tempo(s)" > saida_$MODO.csv
-for ((i = 10; i <= 1000000; i*=10))
+for ((i = 10; i <= 100000; i*=10))
   do
+    echo "Executando para $i aulas"
     echo -n "$i,"  >> saida_$MODO.csv
     ./greedy.x $DIR/Aula$i.txt $MODO >> saida_$MODO.csv
     echo >> saida_$MODO.csv
@@ -12,6 +13,7 @@ for ((i = 10; i <= 1000000; i*=10))
 
 for ((i = 150; i <= 150000; i*=10))
   do
+    echo "Executando para $i aulas"
     echo -n "$i,"  >> saida_$MODO.csv
     ./greedy.x $DIR/Aula$i.txt $MODO >> saida_$MODO.csv
     echo >> saida_$MODO.csv
@@ -19,6 +21,7 @@ for ((i = 150; i <= 150000; i*=10))
 
 for ((i = 200; i <= 20000; i*=10))
   do
+    echo "Executando para $i aulas"
     echo -n "$i," >> saida_$MODO.csv
     ./greedy.x $DIR/Aula$i.txt $MODO >> saida_$MODO.csv
     echo >> saida_$MODO.csv
@@ -26,6 +29,7 @@ for ((i = 200; i <= 20000; i*=10))
 
 for ((i = 25; i <= 250000; i*=100))
   do
+    echo "Executando para $i aulas"
     echo -n "$i," >> saida_$MODO.csv
     ./greedy.x $DIR/Aula$i.txt $MODO >> saida_$MODO.csv
     echo >> saida_$MODO.csv
@@ -33,16 +37,19 @@ for ((i = 25; i <= 250000; i*=100))
 
 for ((i = 300; i <= 30000; i*=100))
   do
+    echo "Executando para $i aulas"
     echo -n "$i," >> saida_$MODO.csv
     ./greedy.x $DIR/Aula$i.txt $MODO >> saida_$MODO.csv
     echo >> saida_$MODO.csv
   done
 
+echo "Executando para 350000 aulas"
 echo -n "350000,"  >> saida_350000.csv
-./greedy.x $DIR/Aula350000.txt $MODO >> saida_350000.csv
+./greedy.x $DIR/Aula350000.txt $MODO >> saida_$MODO.csv
 
 for ((i = 50; i <= 500000; i*=10))
   do
+    echo "Executando para $i aulas"
     echo -n "$i," >> saida_$MODO.csv
     ./greedy.x $DIR/Aula$i.txt $MODO >> saida_$MODO.csv
     echo >> saida_$MODO.csv
@@ -50,6 +57,7 @@ for ((i = 50; i <= 500000; i*=10))
 
 for ((i = 750; i <= 750000; i*=10))
   do
+    echo "Executando para $i aulas"
     echo -n "$i,"  >> saida_$MODO.csv
     ./greedy.x $DIR/Aula$i.txt $MODO >> saida_$MODO.csv
     echo >> saida_$MODO.csv 
