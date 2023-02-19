446f441a91a331b5f443b3c1de6f44529014355b
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2023-02-19 17:11:59 -0300

    fix: run script

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 # Script to run the greedy algorithm for all the input files and modes
 # Limpando arquivos de saida
 echo "Limpando arquivos de saida..."
-echo "" > saida_start.csv
+echo "" > saida_1.csv
 echo "" > saida_2.csv
-echo "Executando modo start para todos os arquivos..."
-./script.sh start
+echo "Executando modo 1 para todos os arquivos..."
+./script.sh 1
 echo "Executando modo 2 para todos os arquivos..."
 ./script.sh 2
